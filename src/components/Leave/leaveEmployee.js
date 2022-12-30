aa4f30fc7c171de82f21e96e29b35dd56b268e0c
amisha <amisha9565@gmail.com>
2022-12-30 14:43:10 +0530

    fixed the issues of table in leaveEmployee

--- src/components/Leave/leaveEmployee.js
+++ src/components/Leave/leaveEmployee.js
@@ -100,7 +100,7 @@ const EmployeeLeaveComponent = () => {
                         <h2>Leaves</h2>
                     </div>
                     <div className="col-md-6">
-                        <button className={`${styles.leaveSubmit} btn btn-primary mx-1 primary-button-bg float-end`} onClick={openLeaveModal} >
+                        <button className={`${styles.leaveSubmit} btn btn-primary mx-1 float-end textFont`} onClick={openLeaveModal} >
                             Add leave
                         </button>
                     </div>
@@ -110,7 +110,7 @@ const EmployeeLeaveComponent = () => {
                         <div className="card order-card shadow border-1">
                             <div className="card-block text-dark text-center">
                                 <h6 className="m-b-20">Annual Leave</h6>
-                                <h3>12</h3>
+                                <h4>12</h4>
                             </div>
                         </div>
                     </div>
@@ -118,7 +118,7 @@ const EmployeeLeaveComponent = () => {
                         <div className="card order-card shadow border-1">
                             <div className="card-block text-dark text-center">
                                 <h6 className="m-b-20">Medical Leave</h6>
-                                <h3>1</h3>
+                                <h4>1</h4>
                             </div>
                         </div>
                     </div>
@@ -126,7 +126,7 @@ const EmployeeLeaveComponent = () => {
                         <div className="card order-card shadow border-1">
                             <div className="card-block text-dark text-center">
                                 <h6 className="m-b-20">Other Leave</h6>
-                                <h3>5</h3>
+                                <h4>5</h4>
                             </div>
                         </div>
                     </div>
@@ -134,7 +134,7 @@ const EmployeeLeaveComponent = () => {
                         <div className="card order-card shadow border-1">
                             <div className="card-block text-dark text-center">
                                 <h6 className="m-b-20">Remaining Leave</h6>
-                                <h3>6</h3>
+                                <h4>6</h4>
                             </div>
                         </div>
                     </div>
@@ -230,11 +230,11 @@ const EmployeeLeaveComponent = () => {
                                                     title="View Actions"
                                                     className={`bg-white border-0 p-0`}
                                                 >
-                                                    <Image src={'/images/three-dot-icon.svg'} alt="" className="cursor-pointer mb-1 " />
+                                                    <Image src={'/images/three-dot-icon.svg'} alt="" className="cursor-pointer mb-1 sortImg" />
                                                     {/* <Image src={'/images/more.png'} className="cursor-pointer mb-1 sortImg " alt="" /> */}
                                                 </Dropdown.Toggle>
 
-                                                <Dropdown.Menu>
+                                                <Dropdown.Menu className="textFont">
                                                     <Dropdown.Item >
                                                         <span>Edit</span>
                                                     </Dropdown.Item>
