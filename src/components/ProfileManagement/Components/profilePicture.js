ea3d67d2312317badba329d59388fe6d371db446
amisha <amisha9565@gmail.com>
2023-01-12 15:44:04 +0530

    fetched the url of image from post and set it to setSrc

--- src/components/ProfileManagement/Components/profilePicture.js
+++ src/components/ProfileManagement/Components/profilePicture.js
@@ -10,15 +10,14 @@ import { Input } from "reactstrap";
 import { IconPencil } from "@tabler/icons";
 import { useDispatch, useSelector } from "react-redux";
 import { setProfilePicture } from "../../../stores/actions/profilePicture";
-import Avatar from 'react-avatar-edit';
 
-export default function ProfilePictureComponent() {
+export default function ProfilePictureComponent({setSrc}) {
+
   const [imageCrop, setimageCrop] = useState();
   const [images, setImages] = useState([]);
   const [imageURls, setImageURls] = useState([]);
   const [selectedFile, setSelectedFile] = useState();
   const dispatch = useDispatch();
-  const [preview, setPreview] = useState(false);
 
   const userData = useSelector((Gstate) => Gstate.user.userData);
   const imagePath = userData?.Image?.split('/');
@@ -47,11 +46,11 @@ export default function ProfilePictureComponent() {
 
   const uploadPic = () => {
     setimageCrop(false);
-    // console.log(preview)
     const formData = new FormData();
-    formData.append('File', preview);
+    formData.append('File', selectedFile);
     API.apiPost("userProfilePicture", formData)
       .then((response) => {
+        setSrc( response?.request?.responseURL)
         if (response.data) {
           toast.success("Image submitted successfully, kindly refresh the page to update your profile picture", {
             position: "top-right",
@@ -67,39 +66,11 @@ export default function ProfilePictureComponent() {
       .catch((err) => {
         handleErrorMessage(err);
       });
-  }  
+  }
 
   const getPic = () => {
     dispatch(setProfilePicture(imagePathFilter));
   }
-  const uploadImage = () =>{
-    API.apiPost("userProfilePicture", {payload: encodeData()})
-      .then((response) => {
-        if (response.data && response.data.success === true) {
-          toast.success(response.data.message, {position: "top-right"});
-        }
-      })
-      // .catch((err) => {
-      //   handleErrorMessage(err);
-      // });
-  }
-
-  const onCrop = (view) => {
-    // var src = window.URL.createObjectURL(view);
-    // console.log('src', src)
-    var binaryData = [];
-    binaryData.push(view);
-    const cc = window.URL.createObjectURL(new Blob(binaryData, { type: "application/zip" }))
-    console.log('window.URL.createObjectURL(new Blob(binaryData, {type: "image/jpeg/png/jpg"}))', cc)
-
-    console.log('binaryData', binaryData)
-
-    setPreview(cc);
-  };
-
-  const onClose = () => {
-    setPreview(null);
-  };
 
   return (
     <>
@@ -108,9 +79,9 @@ export default function ProfilePictureComponent() {
       <label htmlFor="avatar" className="d-flex justify-content-center" onClick={() => setimageCrop(true)}>
         Choose a profile picture:
       </label>
-      <div className="d-flex justify-content-center my-4">
+      <div className={`d-flex justify-content-center my-4 `}>
         <div
-          className={`d-flex flex-column justify-content-center algin-items-center ${styles.imageDiv}`}
+          className={`d-flex flex-column justify-content-center align-items-center ${styles.imageDiv} `}
         >
           <IconPencil onClick={() => setimageCrop(true)} className={`${styles.editIcon}`} />
           {
@@ -118,7 +89,7 @@ export default function ProfilePictureComponent() {
               imageURls.map((imageSrc, i) =>
                 // eslint-disable-next-line @next/next/no-img-element
                 <img key={i}
-                  className="w-100"
+                  className={`w-100 ${styles.userProfile}`}
                   src={imageSrc}
                   alt=""
                 />
@@ -127,6 +98,7 @@ export default function ProfilePictureComponent() {
               <img
                 alt="hh"
                 src={upload}
+                className={`${styles.userProfile}`}
               />
           }
           <Modal
@@ -140,7 +112,7 @@ export default function ProfilePictureComponent() {
               <Modal.Title>Update Profile</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              {/* <div className="d-flex flex-column align-items-center">
+              <div className="d-flex flex-column align-items-center">
                 <Input type="file" multiple accept="image/*" onChange={changeHandler} />
                 {selectedFile && <div className="row">
                   <Card className='w-25 m-3'>
@@ -155,16 +127,6 @@ export default function ProfilePictureComponent() {
                   </Card>
                 </div>
                 }
-              </div> */}
-              <div className="d-flex flex-column align-items-center">
-                <Avatar
-                  width={300}
-                  height={300}
-                  onCrop={onCrop}
-                  onClose={onClose}
-                //onBeforeFileLoad={onFileLoad}
-                //src={src}
-                />
               </div>
             </Modal.Body>
             <Modal.Footer className="d-flex justify-content-center">
